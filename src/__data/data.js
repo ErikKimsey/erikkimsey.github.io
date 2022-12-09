7d8e8359c1c60c6057004dcd4f811cb54f886be0
Erik <kimseyerik@gmail.com>
2022-12-08 19:36:41 -0500

    edit to SELF STATEMENT

--- src/__data/data.js
+++ src/__data/data.js
@@ -1,100 +1,100 @@
 // The document contains data used to populate fields throughout the site.
 // Google News API key: 670f9cd6f82643a99331cc217f30f479
 
-const SELF_STATEMENT = "Software engineer passionate about building beautiful, dynamic, and intuitive user-interfaces for any and all platforms.";
+const SELF_STATEMENT = "Software engineer passionate about building beautiful, dynamic, and intuitive user-interfaces.";
 
 const LANGUAGES = [
-    'LANGUAGES',
-    'Javascript',
-    'C#',
-    'Typescript',
-    'C++',
-    'Java',
-    'Processing'
+  'LANGUAGES',
+  'Javascript',
+  'C#',
+  'Typescript',
+  'C++',
+  'Java',
+  'Processing'
 ]
 
 const THREE_D = [
-    '3D',
-    'Unity3D',
-    'Blender',
-    'Three.js',
-    'React-Three-Fiber',
-    'Drei',
+  '3D',
+  'Unity3D',
+  'Blender',
+  'Three.js',
+  'React-Three-Fiber',
+  'Drei',
 
 ]
 
 const DESIGN_TOOLS = [
-    'GRAPHIC DESIGN',
-    'Adobe Illustrator',
-    'Adobe Photoshop',
-    'Adobe InDesign',
-    'Procreate',
-    'Figma',
-    'Vectornator',
+  'GRAPHIC DESIGN',
+  'Adobe Illustrator',
+  'Adobe Photoshop',
+  'Adobe InDesign',
+  'Procreate',
+  'Figma',
+  'Vectornator',
 ]
 
 
 const FRONTEND = [
-    'FRONTEND',
-    'JavaScript',
-    'Typescript',
-    'React',
-    'React Native',
-    'HTML5 Canvas',
-    'Sass',
-    'Less',
-    'Framer Motion',
-    'Three.js',
-    'D3.js',
-    'P5.js',
-    'Redux',
+  'FRONTEND',
+  'JavaScript',
+  'Typescript',
+  'React',
+  'React Native',
+  'HTML5 Canvas',
+  'Sass',
+  'Less',
+  'Framer Motion',
+  'Three.js',
+  'D3.js',
+  'P5.js',
+  'Redux',
 ]
 
 const MULTIMEDIA_WEB = [
-    'WEB MULTIMEDIA',
-    "Web Audio API",
-    "WebGL",
+  'WEB MULTIMEDIA',
+  "Web Audio API",
+  "WebGL",
 
 ]
 
 const BACKEND = [
-    'BACKEND',
-    'Node.js / Express.js',
-    'SQL / MySQL / SQLite',
-    'Flask',
-    'Django',
-    'GraphQL',
-    'Redis',
+  'BACKEND',
+  'Node.js / Express.js',
+  'SQL / MySQL / SQLite',
+  'Flask',
+  'Django',
+  'GraphQL',
+  'Redis',
 ]
 
 const AUDIO_VIDEO = [
-    'AUDIO / VIDEO',
-    'Ableton Live',
-    'Propellerhead Reason',
-    'Adobe Premiere',
-    'Adobe After Effects',
-    'Blender',
+  'AUDIO / VIDEO',
+  'Ableton Live',
+  'Propellerhead Reason',
+  'Adobe Premiere',
+  'Adobe After Effects',
+  'Blender',
 ]
 
 const CONTACT_DATA = {
-    name: 'Erik Kimsey',
-    role: 'Software Engineer',
-    phone: '540-278-4525',
-    email_1: 'kimseyerik',
-    email_2: 'gmail[dot]com'
+  name: 'Erik Kimsey',
+  role: 'Software Engineer',
+  phone: '540-278-4525',
+  email_1: 'kimseyerik',
+  email_2: 'gmail[dot]com'
 };
 
 const THE_WHO_AM_I = [
-    'Software Engineer',
-    'Frontend Developer',
-    'Creative Technologist',
-    'Multimedia Artist',
-    'Musician',
-    'Half human, half coffee',
+  'Software Engineer',
+  'Frontend Developer',
+  'Creative Technologist',
+  'Multimedia Artist',
+  'Musician',
+  'Half human, half coffee',
 ]
 
 export const SKILLS = [
-    LANGUAGES, FRONTEND, BACKEND, DESIGN_TOOLS, THREE_D, AUDIO_VIDEO, MULTIMEDIA_WEB
+  LANGUAGES, FRONTEND, BACKEND, DESIGN_TOOLS, THREE_D, AUDIO_VIDEO, MULTIMEDIA_WEB
 ];
 
 export { LANGUAGES, FRONTEND, BACKEND, DESIGN_TOOLS, THREE_D, CONTACT_DATA, AUDIO_VIDEO, MULTIMEDIA_WEB, THE_WHO_AM_I, SELF_STATEMENT }
