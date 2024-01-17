90ddd565bd02e15533f9e832b1bd613f24dbac83
Erik <kimseyerik@gmail.com>
2024-01-17 12:08:58 -0500

    Finessing a bunch of styles in Banner and Skillset

--- src/Banner/BannerMenu.jsx
+++ src/Banner/BannerMenu.jsx
@@ -8,85 +8,85 @@ import "./bannerMenu.scss";
 // ]
 
 const variants = {
-  transition: {
-    duration: 0.8,
-    delay: 1.4,
-    ease: [0, 0.71, 0.2, 1.01],
-    y: { stiffness: 1000 }
-  }
+    transition: {
+        duration: 0.8,
+        delay: 1.4,
+        ease: [0, 0.71, 0.2, 1.01],
+        y: { stiffness: 1000 }
+    }
 }
 
 
 
 function BannerMenuItem(props) {
-  let { tag, link, thisRef } = props;
+    let { tag, link, thisRef } = props;
 
-  useEffect(() => {
-    let sk;
-    if (document.querySelector(`.${link}`) !== null) {
-      sk = document.querySelector(`.${link}`);
+    useEffect(() => {
+        let sk;
+        if (document.querySelector(`.${link}`) !== null) {
+            sk = document.querySelector(`.${link}`);
+        }
+    }, [])
+
+    function scrollToPage() {
+        if (link === undefined) {
+            return;
+        }
+        let y, x;
+        let el = document.getElementById(link);
+        if (el !== undefined) {
+            y = el.offsetTop;
+            x = el.offsetLeft;
+            window.scrollTo({ left: x, top: y, behavior: "smooth" });
+        }
     }
-  }, [])
 
-  function scrollToPage() {
-    if (link === undefined) {
-      return;
-    }
-    let y, x;
-    let el = document.getElementById(link);
-    if (el !== undefined) {
-      y = el.offsetTop;
-      x = el.offsetLeft;
-      window.scrollTo({ left: x, top: y, behavior: "smooth" });
-    }
-  }
+    return (
+        <motion.div
+            ref={thisRef}
+            className="bannerMenuItem"
+            variants={variants}
+            whileHover={{ scale: 1.1, color: "rgba(0,0,0,0)", textShadow: "3px 2px 3px rgba(232, 12, 122, 0.6), -3px 2px 4px rgba(105, 55, 245, 0.6)" }}
+            whileTap={{ scale: 0.95 }}
+            onClick={scrollToPage}
+        >
+            {tag}
+        </motion.div>
 
-  return (
-    <motion.div
-      ref={thisRef}
-      className="bannerMenuItem"
-      variants={variants}
-      whileHover={{ scale: 1.1, color: "#FFFEC8" }}
-      whileTap={{ scale: 0.95 }}
-      onClick={scrollToPage}
-    >
-      {tag}
-    </motion.div>
-
-  )
+    )
 }
 
 export default function BannerMenu() {
-  let itemLinks = [
-    { id: "skillset-container", tag: "skill set", thisRef: useRef() },
-    { id: "experience-container", tag: "experience", thisRef: useRef() },
-  ];
+    let itemLinks = [
+        { id: "skillset-container", tag: "skill set", thisRef: useRef() },
+        { id: "experience-container", tag: "experience", thisRef: useRef() },
+    ];
 
-  let [menuLinks, setMenuLinks] = useState();
+    let [menuLinks, setMenuLinks] = useState();
 
-  useEffect(() => {
-    if (itemLinks && itemLinks.length > 0) {
-      setMenuLinks(itemLinks);
-    }
-  }, [])
+    useEffect(() => {
+        if (itemLinks && itemLinks.length > 0) {
+            setMenuLinks(itemLinks);
+        }
+    }, [])
 
-  return (
-    <motion.div className="bannerMenu"
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 0.8,
-        delay: 3,
-        ease: [0, 0.71, 0.2, 1.01]
-      }}
-    >
-      {menuLinks &&
-        menuLinks.map((e, i) => {
-          return (
-            <BannerMenuItem tag={e.tag} link={e.id} key={i} thisRef={e.thisRef} />
-          )
-        })
-      }
-    </motion.div>
-  )
+    return (
+        <motion.div className="bannerMenu"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 0.8,
+                delay: 3,
+                ease: [0, 0.71, 0.2, 1.01]
+            }}
+        >
+            {menuLinks &&
+                menuLinks.map((e, i) => {
+                    return (
+                        <BannerMenuItem tag={e.tag} link={e.id} key={i} thisRef={e.thisRef} />
+                    )
+                })
+            }
+        </motion.div>
+    )
 }
--- src/__data/data.js
+++ src/__data/data.js
@@ -4,83 +4,81 @@
 const SELF_STATEMENT = "Software engineer passionate about building beautiful, dynamic, and intuitive user-interfaces.";
 
 const LANGUAGES = [
-  'LANGUAGES',
-  'Javascript / Typescript',
-  'C#',
-  'Swift',
-  'Python',
-  'C / C++',
-  'LOLCODE'
+    'LANGUAGES',
+    'Javascript / Typescript',
+    'C#',
+    'Swift',
+    'Python',
+    'C / C++',
 ]
 
 const THREE_D = [
-  '3D TOOLS',
-  'Three.js',
-  'React-Three-Fiber / Drei',
-  'Unity3D',
-  'Blender'
+    '3D TOOLS',
+    'Three.js',
+    'React-Three-Fiber / Drei',
+    'Unity3D',
+    'Blender'
 ]
 
 const DESIGN_TOOLS = [
-  'DESIGN',
-  'Adobe Illustrator',
-  'Adobe Photoshop',
-  'Procreate',
-  'Figma',
+    'DESIGN',
+    'Adobe Illustrator',
+    'Adobe Photoshop',
+    'Procreate',
+    'Figma',
 ]
 
 
 const FRONTEND = [
-  'FRONTEND / UI DEV',
-  'React / Next.js',
-  'React Native',
-  'C# / Unity',
-  'SwiftUI',
-  'CSS / Sass / Less / Tailwind / Material',
-  'Framer Motion',
-  'Three.js / React-Three-Fiber',
-  'P5.js'
+    'FRONTEND / UI DEV',
+    'React / Next.js',
+    'React Native',
+    'C# / Unity',
+    'SwiftUI',
+    'CSS / Sass / Tailwind',
+    'Framer Motion',
+    'Three.js / RTF',
 ]
 
 const MULTIMEDIA_WEB = [
-  'WEB MULTIMEDIA',
-  "Web Audio API",
-  "WebGL",
+    'WEB MULTIMEDIA',
+    "Web Audio API",
+    "WebGL",
 ]
 
 const BACKEND = [
-  'BACKEND',
-  'Node.js / Express.js',
-  'SQL / MySQL / SQLite',
-  'GraphQL / Apollo',
-  'Django'
+    'BACKEND',
+    'Node.js / Express.js',
+    'SQL / MySQL / SQLite',
+    'GraphQL / Apollo',
+    'Django'
 ]
 
 const AUDIO_VIDEO = [
-  'AUDIO / VIDEO',
-  'Ableton Live',
-  'Propellerhead Reason'
+    'AUDIO / VIDEO',
+    'Ableton Live',
+    'Propellerhead Reason'
 ]
 
 const CONTACT_DATA = {
-  name: 'Erik Kimsey',
-  role: 'Software Engineer',
-  phone: '540-278-4525',
-  email_1: 'kimseyerik',
-  email_2: 'gmail[dot]com'
+    name: 'Erik Kimsey',
+    role: 'Software Engineer',
+    phone: '540-278-4525',
+    email_1: 'kimseyerik',
+    email_2: 'gmail[dot]com'
 };
 
 const THE_WHO_AM_I = [
-  'Software Engineer',
-  'Frontend Developer',
-  'Creative Technologist',
-  'Multimedia Artist',
-  'Musician',
-  'Half human, half coffee',
+    'Software Engineer',
+    'Frontend Developer',
+    'Creative Technologist',
+    'Multimedia Artist',
+    'Musician',
+    'Half human, half coffee',
 ]
 
 export const SKILLS = [
-  LANGUAGES, FRONTEND, THREE_D, BACKEND, DESIGN_TOOLS,
+    LANGUAGES, FRONTEND, THREE_D, BACKEND, DESIGN_TOOLS,
 ];
 
 export { LANGUAGES, FRONTEND, BACKEND, DESIGN_TOOLS, THREE_D, CONTACT_DATA, AUDIO_VIDEO, MULTIMEDIA_WEB, THE_WHO_AM_I, SELF_STATEMENT }
