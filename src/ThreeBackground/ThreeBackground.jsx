8a77618cf3cf2c18fb88651f27fd6dfa6bc441bf
Erik <kimseyerik@gmail.com>
2024-05-31 11:39:54 -0400

    Created a new model for ThreeBackground. . Added Leva to control scene from browser.

--- src/ThreeBackground/ThreeBackground.jsx
+++ src/ThreeBackground/ThreeBackground.jsx
@@ -1,97 +1,86 @@
 import * as THREE from 'three';
-import { useState, useRef, useEffect, Suspense } from 'react';
+import { useRef, Suspense, useMemo } from 'react';
 import { Canvas, useLoader, useFrame, extend, useThree } from '@react-three/fiber'
-import { Environment } from '@react-three/drei';
-import { OrbitControls } from '@react-three/drei';
-import { EffectComposer, DepthOfField, Vignette, Bloom } from '@react-three/postprocessing'
+import { OrbitControls, Environment } from '@react-three/drei';
+import { EffectComposer, DepthOfField, Bloom } from '@react-three/postprocessing'
 import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader';
 import { useControls } from 'leva'
-import Loading from "../Loading/Loading";
+
 
 import './threebkgrd.css';
 
 extend({ OrbitControls })
 
-const Controls = () => {
-    const { camera, gl } = useThree()
-    const ref = useRef()
-    useFrame(() => ref.current.update())
-    return <orbitControls ref={ref} target={[0, 0, 0]} enableDamping args={[camera, gl.domElement]} />
-}
+function The3DObject({ }) {
+    const groupRef = useRef();
+    const { nodes, materials } = useLoader(GLTFLoader, '/cube.gltf')
 
-function Sound({ url }) {
-    const sound = useRef()
-    const { camera } = useThree()
-    const [listener] = useState(() => new THREE.AudioListener())
-    const buffer = useLoader(THREE.AudioLoader, url)
-    useEffect(() => {
-        sound.current.setBuffer(buffer)
-        sound.current.setRefDistance(0.1)
-        sound.current.setLoop(true)
-        sound.current.play()
-        camera.add(listener)
-        return () => camera.remove(listener)
+    let cube = nodes.cube.geometry;
+    let sphere = nodes.sphere.geometry;
+    let cubeMat = materials.cube;
+    let sphereMat = materials.sphere;
+
+
+    const options = useMemo(() => {
+        return {
+            x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+            y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+            z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+            visible: true,
+            color: { value: 'lime' },
+        }
     }, [])
-    return <positionalAudio ref={sound} args={[listener]} />
-}
 
-function The3DObject() {
-    const groupRef = useRef()
-    const { nodes, materials } = useLoader(GLTFLoader, '/lowpoly.gltf')
-    let geom = nodes.lowpoly.geometry;
-    let mat = materials.lowpoly;
+    const ctrls = useMemo(() => {
+        return {
+            color: { value: 'black' },
+            x: { value: 0 },
+            y: { value: 0 },
+            z: { value: -3.0 }
+        }
+    }, []);
 
-    // useControls('Ambient Light', {
-    //     visible: {
-    //         value: false,
-    //         onChange: (v) => {
-    //             ambientRef.current.visible = v
-    //         },
-    //     },
-    //     color: {
-    //         value: 'white',
-    //         onChange: (v) => {
-    //             ambientRef.current.color = new THREE.Color(v)
-    //         },
-    //     },
-    // })
+    let cubeCtrls = useControls("Cube", ctrls);
 
+    useFrame((state) => {
+        if (groupRef.current) {
+            groupRef.current.rotation.y += 0.01
+            groupRef.current.rotation.z += 0.01
+        }
+    })
 
     return (
-        <group ref={groupRef} dispose={null} position={[3, -5, -5]} rotation={[20, 70, -110]}>
-            <mesh castShadow receiveShadow geometry={geom} material={mat} />
-            {/* <mesh castShadow receiveShadow geometry={nodes.Curve007_1.geometry} material={materials['Material.001']} />
-            <mesh castShadow receiveShadow geometry={nodes.Curve007_2.geometry} material={materials['Material.002']} /> */}
+        <group dispose={null} ref={groupRef} position={[cubeCtrls.x, cubeCtrls.y, cubeCtrls.z]} rotation={[0, 33, 11]} >
+            <mesh castShadow receiveShadow geometry={cube} material={cubeMat} />
+            <mesh geometry={sphere} material={sphereMat} />
         </group>
     )
-
 }
 
 export default function ThreeBackground() {
 
-    let lightRef = useRef();
 
     return (
         <div className="three-bkgrd-container">
-            <Canvas camera={{ position: [0, 0, 0] }} frameloop="demand">
-                <EffectComposer>
-                    <Suspense>
-                        {/* <Environment files="hdri_env.exr" /> */}
-                        <ambientLight color="pink" intensity={0.1} />
-                        <directionalLight color="rgb(90,60,110)" position={[-30, 0, -30]} intensity={20} />
+            <Suspense>
+                <Canvas camera={{ position: [0, 0, 0] }} frameloop="always">
+                    <EffectComposer>
+                        {/* <Environment files="public/hubble.png" /> */}
+                        {/* <ambientLight color="black" intensity={0.1} /> */}
+                        <directionalLight color="rgb(90,90,90)" position={[-30, 20, -10]} intensity={10} />
                         {/* <fog attach="fog" args={['rgb(22,0,43)', 1, 100]} /> */}
-                        <Bloom luminanceThreshold={0.01} luminanceSmoothing={0.1} height={260} />
-                        <DepthOfField focusDistance={0.09} // where to focus
-                            focalLength={0.05} // focal length 
-                            bokehScale={10} />
+                        <Bloom luminanceThreshold={0.01} luminanceSmoothing={0.01} height={260} />
+                        {/* <DepthOfField focusDistance={0.01} focalLength={0.01} bokehScale={10} /> */}
                         {/* <Vignette
                             darkness={0.6} // vignette darkness
                             eskil={false} // Eskil's vignette technique
                         /> */}
-                        <The3DObject />
-                    </Suspense>
-                </EffectComposer>
-            </Canvas>
+                        <Suspense>
+                            <The3DObject />
+                        </Suspense>
+                    </EffectComposer>
+                </Canvas>
+            </Suspense>
         </div >
     )
 }
