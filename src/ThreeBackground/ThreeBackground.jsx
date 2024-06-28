4fa5a8234a1cd9ff353ee785050c34d4c839ba95
Erik Kimsey <kimseyerik@gmail.com>
2024-06-27 21:30:38 -0400

    Bringing ThreeBackground back to life.

--- src/ThreeBackground/ThreeBackground.jsx
+++ src/ThreeBackground/ThreeBackground.jsx
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
-import { useRef, Suspense, useMemo } from 'react';
+import { useRef, Suspense, useMemo, useState, useEffect } from 'react';
 import { Canvas, useLoader, useFrame, extend, useThree } from '@react-three/fiber'
+import Slider from '@mui/material/Slider';
 import { OrbitControls, Environment } from '@react-three/drei';
 import { EffectComposer, DepthOfField, Bloom } from '@react-three/postprocessing'
 import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader';
@@ -11,82 +12,76 @@ import './threebkgrd.css';
 
 extend({ OrbitControls })
 
-function The3DCube({ }) {
-    const groupRef = useRef();
-    const { nodes, materials } = useLoader(GLTFLoader, '/cube.gltf')
+function The3DCube(props) {
 
-    let cube = nodes.cube.geometry;
-    let cubeMat = materials.cube;
+    const [allPos, setAllPos] = useState([0, 0, 0]);
+    console.log(props.allPos);
 
-    const ctrls = useMemo(() => {
-        return {
-            color: { value: 'black' },
-            x: { value: 0 },
-            y: { value: 0 },
-            z: { value: -3.0 }
-        }
-    }, []);
+    useEffect(() => {
+        setAllPos(props.allPos)
+    }, [])
 
-    let cubeCtrls = useControls("Cube", ctrls);
+    useEffect(() => {
+        setAllPos(props.allPos)
+    }, [props.allPos])
 
-    useFrame((state) => {
-        if (groupRef.current) {
-            groupRef.current.rotation.y += 0.01
-            groupRef.current.rotation.z += 0.01
-        }
-    })
 
     return (
-        <group dispose={null} ref={groupRef} position={[cubeCtrls.x, cubeCtrls.y, cubeCtrls.z]} rotation={[0, 33, 11]} >
-            <mesh castShadow receiveShadow geometry={cube} material={cubeMat} />
-        </group>
-    )
-}
-
-function The3DSphere({ }) {
-    const groupRef = useRef();
-    const { nodes, materials } = useLoader(GLTFLoader, '/cube.gltf')
-
-    let sphere = nodes.sphere.geometry;
-    let sphereMat = materials.sphere;
-
-    const ctrls = useMemo(() => {
-        return {
-            color: { value: 'black' },
-            x: { value: 0 },
-            y: { value: 0 },
-            z: { value: -3.0 }
-        }
-    }, []);
-
-    let cubeCtrls = useControls("Cube", ctrls);
-
-    useFrame((state) => {
-        if (groupRef.current) {
-            groupRef.current.rotation.y += 0.01
-            groupRef.current.rotation.z += 0.01
-        }
-    })
-
-    return (
-        <group dispose={null} ref={groupRef} position={[cubeCtrls.x, cubeCtrls.y, cubeCtrls.z]} rotation={[0, 33, 11]} >
-            <mesh geometry={sphere} material={sphereMat} />
-        </group>
+        <>
+            {allPos &&
+                <mesh visible userData={{ hello: 'world' }} position={[allPos[0], allPos[1], allPos[2]]} rotation={[Math.PI / 2, 0, 0]}>
+                    <sphereGeometry args={[3, 32, 16]} />
+                    <meshLambertMaterial color="hotpink" />
+                </mesh>
+            }
+        </>
     )
 }
 
 export default function ThreeBackground() {
 
+    let [xPos, setXPos] = useState(0);
+    let [yPos, setYPos] = useState(0);
+    let [zPos, setZPos] = useState(0);
+
+    useEffect(() => {
+    }, [xPos, yPos, zPos])
+
+    function handleXInput(e) {
+        setXPos(e.target.value)
+    }
+
+    function handleYInput(e) {
+        setYPos(e.target.value)
+    }
+
+    function handleZInput(e) {
+        setZPos(e.target.value)
+    }
 
     return (
         <div className="three-bkgrd-container">
+            <div className='ctrls'>
+                <section style={{ margin: "2px", border: "solid 1px #555", padding: "5px", borderRadius: "3px" }}>
+                    <label for="volume">{"X: " + xPos}</label>
+                    <input type="range" id="volume" name="xPos" min="-10" max="11" value={xPos} onChange={handleXInput} />
+                </section>
+
+                <section style={{ margin: "2px", border: "solid 1px #555", padding: "5px", borderRadius: "3px" }}>
+                    <label for="volume">{"Y: " + yPos}</label>
+                    <input type="range" id="volume" name="yPos" min="-10" max="11" value={yPos} onChange={handleYInput} />
+                </section>
+                <section style={{ margin: "2px", border: "solid 1px #555", padding: "5px", borderRadius: "3px" }}>
+                    <label for="volume">{"Z: " + zPos}</label>
+                    <input type="range" id="volume" name="zPos" min="-10" max="11" value={zPos} onChange={handleZInput} />
+                </section>
+            </div>
+
             <Suspense>
-                <Canvas camera={{ position: [0, 0, 0] }} frameloop="always">
+                <Canvas camera={{ position: [0, 0, (-12)] }} frameloop="always">
                     <EffectComposer>
-                        {/* <Environment files="public/hubble.png" /> */}
-                        {/* <ambientLight color="black" intensity={0.1} /> */}
-                        <directionalLight color="rgb(90,90,90)" position={[-30, 20, -10]} intensity={10} />
-                        {/* <fog attach="fog" args={['rgb(22,0,43)', 1, 100]} /> */}
+                        <directionalLight color="rgb(255,255,255)" position={[10, 5, -10]} intensity={21} />
+                        <fog attach="fog" args={['rgb(22,0,43)', 0, 100]} />
                         <Bloom luminanceThreshold={0.01} luminanceSmoothing={0.01} height={260} />
                         {/* <DepthOfField focusDistance={0.01} focalLength={0.01} bokehScale={10} /> */}
                         {/* <Vignette
@@ -94,12 +89,11 @@ export default function ThreeBackground() {
                             eskil={false} // Eskil's vignette technique
                         /> */}
                         <Suspense>
-                            <The3DCube />
-                            <The3DSphere />
+                            <The3DCube allPos={[xPos, yPos, zPos]} />
                         </Suspense>
                     </EffectComposer>
                 </Canvas>
-            </Suspense>
+            </Suspense >
         </div >
     )
 }
