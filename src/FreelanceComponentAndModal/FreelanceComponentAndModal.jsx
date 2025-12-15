7749a1c2b8b7beb459b0ea7cadfb56bc6fd99931
Erik Kimsey <kimseyerik@gmail.com>
2025-12-15 09:08:24 -0500

    completed deal package design.  Adjust modal bkgrd, added new packages

--- src/FreelanceComponentAndModal/FreelanceComponentAndModal.jsx
+++ src/FreelanceComponentAndModal/FreelanceComponentAndModal.jsx
@@ -6,22 +6,32 @@ import { motion, AnimatePresence } from "framer-motion";
 const DEALS_PACKAGES = [
     {
         label: "UI Fixes and Adjustments",
-        price: "$150",
+        price: "$200",
         description: "letcher cybernetic mimetic quicksilver dub quartz-halogen origami grid temperfoam polysaccharide mycotoxin program code infrastructure cop. "
     },
     {
         label: "UI / UX Design",
         price: "$150",
-        description: "	Multinational chip prototype polysaccharide neural SAS origami clinic cryptic black zaibatsu cutting-edge cop Yakuza. "
+        description: "Multinational chip prototype polysaccharide neural SAS origami clinic cryptic black zaibatsu cutting-edge cop Yakuza. "
     },
     {
-        label: "Prototyping : Mobile Game (single scene)",
+        label: "Prototyping : Mobile Game",
+        price: "$350",
+        description: "Helsinki Sprawl factory blue trace quicksilver ICE shattered military. Ninja socket temperfoam samurai ICE implant laser-scrawled software trace cabal Chinese chrome. "
+    },
+    {
+        label: "Prototyping : Web App",
         price: "$300",
         description: "Helsinki Sprawl factory blue trace quicksilver ICE shattered military. Ninja socket temperfoam samurai ICE implant laser-scrawled software trace cabal Chinese chrome. "
     },
     {
-        label: "Prototyping : Web App (single screen)",
+        label: "3D Modeling",
         price: "$300",
+        description: "Helsinki Sprawl factory blue trace quicksilver ICE shattered military. Ninja socket temperfoam samurai ICE implant laser-scrawled software trace cabal Chinese chrome. "
+    },
+    {
+        label: "Sound FX Design",
+        price: "$150",
         description: "Helsinki Sprawl factory blue trace quicksilver ICE shattered military. Ninja socket temperfoam samurai ICE implant laser-scrawled software trace cabal Chinese chrome. "
     }
 ]
@@ -31,28 +41,17 @@ function DealPackage({ index, pack }) {
     const { label, price, description } = pack;
 
     return (
-        index % 2 !== 0 ?
-            (<div className="relative flex flex-wrap justify-between black p-2 m-4 h-72
+        // index % 2 !== 0 ?
+        (<div className="relative flex flex-wrap justify-between black p-2 m-4 h-72 sm:w-96 md:w-96 lg:w-96 min-w-70
              border-2 rounded-md  border-stone-400">
-                <div className="flex flex-col w-full sm:w-full  md:w-1/2  lg:w-1/2 ">
-                    <h1 className=" text-peach text-2xl font-quantify font-bold">{label}</h1>
-                    <div className="text-black">{description}</div>
-                </div>
-                <div className="flex flex-col w-full sm:w-full md:w-1/2 lg:w-1/2   p-0 m-0 ">
-                    <div className="text-black font-bold absolute bottom-2 right-2">{price}</div>
-                </div>
-            </div>)
-            :
-            (<div className="relative flex flex-wrap justify-between bg-white p-4 m-4  h-72 border-2 rounded-md border-purpz">
-
-                <div className="flex flex-col w-full sm:w-full  md:w-1/2 lg:w-1/2  ">
-                    <div className="text-blaq font-bold text-right absolute bottom-2 right-2">{price}</div>
-                </div>
-                <div className="flex flex-col w-full sm:w-full  md:w-1/2  lg:w-1/2 p-2">
-                    <h1 className="  text-bluez text-2xl font-quantify font-bold ">{label}</h1>
-                    <div className=" text-blaq font-bold ">{description}</div>
-                </div>
-            </div>)
+            <div className="flex flex-col">
+                <h1 className=" text-peach text-2xl font-quantify font-bold">{label}</h1>
+                <div className="text-black">{description}</div>
+            </div>
+            <div className="flex flex-col w-full sm:w-full md:w-1/2 lg:w-1/2 p-0 m-0 ">
+                <div className="text-black font-bold absolute bottom-2 right-4 font-quantify">{price}</div>
+            </div>
+        </div>)
     )
 }
 
@@ -80,14 +79,14 @@ function FreelanceComponentAndModal() {
             <AnimatePresence>{
                 freelance &&
                 <motion.div
-                    className="fixed flex flex-col justify-center items-center z-[10] top-0 left-0 w-full h-full"
+                    className="fixed flex flex-row flex-wrap justify-center items-center z-[10] top-0 left-0 w-full h-full "
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}>
 
 
                     <motion.div
-                        className="absolute inset-0 bg-black/70 backdrop-blur-md"
+                        className="absolute inset-0 bg-white/20 backdrop-blur-md "
                         onClick={closeModal}
                         aria-hidden="true"
                         initial={{ opacity: 0 }}
@@ -95,7 +94,7 @@ function FreelanceComponentAndModal() {
                         exit={{ opacity: 0 }}
                     />
                     <motion.div
-                        className="relative flex flex-row flex-wrap z-50 h-full max-h-[85vh] w-[90vw] max-w-5xl overflow-auto rounded-lg shadow-2xl bg-black bg-opacity-10  border border-white/10 scroll overflow-x-clip p-10"
+                        className="relative flex flex-row flex-wrap z-50 h-full max-h-[85vh] w-[95vw] max-w-5xl overflow-auto rounded-lg shadow-2xl bg-black bg-opacity-70 inset-shadow-indigo-500 scroll overflow-x-clip p-2 lg:p-10"
                         role="dialog"
                         aria-modal="true"
                         initial={{ y: 24, scale: 0.1, opacity: 0 }}
@@ -104,9 +103,9 @@ function FreelanceComponentAndModal() {
                         transition={{ type: "spring", stiffness: 380, damping: 30 }}
                     >
                         {/* <div className=" w-1/2 h-1/2 bg-fuchsia-900"> */}
-                        <div className="px-10 text-3xl text-center">Cigarette cybernetics deficiency virus gunship segment deficiency prototype. </div>
+                        <div className="px-10 sm:px-2 text-xl lg:text-3xl text-center">Cigarette cybernetics deficiency virus gunship segment deficiency prototype. </div>
                         {/* </div> */}
-                        <div className="relative w-full flex flex-col flex-wrap justify-center ">
+                        <div className="relative w-full flex flex-row flex-wrap justify-center ">
                             {
                                 DEALS_PACKAGES.map((e, i) => {
                                     return <DealPackage index={i} pack={e} />
