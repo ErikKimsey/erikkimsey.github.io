b7366cc9079ac47570af7e78371d49743c6cb7ac
Erik Kimsey <kimseyerik@gmail.com>
2026-01-27 10:46:35 -0500

    Fixing responsive styles in Experience component

--- src/Experience/Experience.jsx
+++ src/Experience/Experience.jsx
@@ -4,6 +4,8 @@ import { motion } from "framer-motion";
 import "./styles/experience.scss";
 import { EXPERIENCE_DATA } from "./experience_data";
 import ExperienceItem from './ExperienceItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Experience() {
@@ -29,15 +31,15 @@ export default function Experience() {
             exit={{ opacity: 0 }}
             transition={{ duration: 0.25, ease: 'easeInOut' }}
         >
-            <div className='w-full flex items-center justify-between px-6  py-4 '>
-                <div className='h-full flex justify-start size-9 text-6xl overflow-visible font-quantify'>Experience</div>
+            <div className='w-full flex flex-col items-start justify-between flex-wrap pl-2 py-4'>
                 <button
                     type="button"
-                    className='px-4 py-2 rounded-md border border-slate-400 text-slate-100 hover:bg-slate-700 transition-colors'
+                    className='pr-4 py-2 mb-12 rounded-md border border-blaq transition-colors'
                     onClick={() => navigate('/')}
                 >
-                    Back to Home
+                    <FontAwesomeIcon icon={faAngleLeft} className="h-full text-xl text-offWhitez hover:text-purps" />
                 </button>
+                <div className='h-full flex justify-start size-9 text-6xl overflow-visible font-quantify'>Experience</div>
             </div>
             <div className="experienceItemsList w-full">
                 {
