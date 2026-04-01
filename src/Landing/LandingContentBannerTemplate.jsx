c82a28c617c7c30f86fbcc2d8d4d7f0f399f2770
Erik Kimsey <kimseyerik@gmail.com>
2026-04-01 14:55:08 -0400

    ADjusted styles on landing

--- src/Landing/LandingContentBannerTemplate.jsx
+++ src/Landing/LandingContentBannerTemplate.jsx
@@ -7,8 +7,10 @@ const LandingContentBannerTemplate = ({ children }) => {
 
 
     return (
-        <div className="fixed z-0 top-20 left-0 w-auto sm:w-full h-full px-2 lg:px-10 flex flex-row flex-wrap overflow-clip">
-            {children}
+        <div className="fixed z-0 top-0 left-0 w-auto sm:w-full h-full px-2 lg:px-10 flex flex-row justify-end flex-wrap overflow-clip">
+            <div className=" z-0 left-0 w-auto sm:w-full px-2 lg:px-10 flex justify-end items-center overflow-clip">
+                {children}
+            </div>
 
         </div>
     )
