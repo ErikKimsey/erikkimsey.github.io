0db97047e93b58ef4ef3a206be8da8747d5accaa
Erik Kimsey <kimseyerik@gmail.com>
2026-04-23 10:50:55 -0400

    Tried dynamically adjusting the speed of the marquee

--- src/SeekingWork/SeekingWork.jsx
+++ src/SeekingWork/SeekingWork.jsx
@@ -6,15 +6,6 @@ import './seeking-work.css'
 const SeekingWork = () => {
 
     const hasAnimated = localStorage.getItem('bannerAnimated') === 'false';
-    const [isVertical, setIsVertical] = useState(false);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setIsVertical(prev => !prev);
-        }, 5000);
-
-        return () => clearInterval(interval);
-    }, [])
 
 
     return <motion.div
@@ -24,13 +15,13 @@ const SeekingWork = () => {
             duration: 0.5,
             delay: 1.5,
         }}
-        className='container fixed bottom-16 w-11/12 md:w-2/3 md:bottom-3 lg:w-full lg:top-10 lg:w-11/12 px-1 lg:px-10 flex flex-row items-center justify-center rounded-sm'>
+        className='container fixed bottom-16 w-11/12 md:w-2/3 md:bottom-3 lg:top-10 lg:w-9/12 px-1 lg:px-10 flex flex-row items-center justify-center rounded-sm'>
 
         <Marquee
-            speed={40}
+            speed='80'
         >
             <motion.p
-                className="w-full text-yellowz overflow-clip text-center p-0 text-2xl font-quantify">
+                className={`w-full text-yellowz overflow-clip text-center p-0 text-2xl font-quantify`}>
                 I am currently available for full-time employment and contract work. &nbsp;&nbsp;
             </motion.p>
             <motion.p
